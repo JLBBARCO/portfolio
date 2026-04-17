9c693b6322b259c5de9744e47bf6ecd249375502
Jose Luiz Bruiani Barco <159829462+JLBBARCO@users.noreply.github.com>
2026-04-17 15:58:08 -0300

    Update projects.json

--- src/js/contact.js
+++
@@ -1,8 +0,0 @@
-// DEPRECATED: Contact functionality has been integrated into footer.js
-// This file is kept for backward compatibility only.
-// The setIconsContact function is now part of the footer() function.
-// Do not use this file directly.
-
-console.warn(
-  "contact.js is deprecated. Contact functionality has been moved to footer.js",
-);
